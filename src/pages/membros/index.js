77ba4ec9950c3270ed925c19f7b629a8c185ee8e
ArthurEly <69876980+Projetos-IDE@users.noreply.github.com>
2023-05-23 15:38:19 -0300

    mudando o tipo de extensão

--- src/pages/membros/index.js
+++ src/pages/membros/index.js
@@ -2,6 +2,7 @@ import fs from 'fs'
 import matter from 'gray-matter'
 import Head from 'next/head'
 import styles from '../../styles/Membros.module.css'
+import { handleJSONfiles } from '../../../functions/jsonHandler'
 
 export default function Membros({ membros }) {
     return (
@@ -28,34 +29,9 @@ export default function Membros({ membros }) {
 }
 
 export async function getStaticProps(){
-    //adicione aqui da onde quer pegar os arquivos
-    // @nome - nome que vai ser chamado na função, ali em cima
-    // @caminho - caminho do diretorio, partindo da raiz
-    const diretorios = [
-      {nome: 'membros', caminho: 'membros_projetos'}
-    ]
+  const membros = handleJSONfiles("./content/membros_projetos");
   
-    const raiz = process.env.PWD;
-    var props = {}
-  
-    diretorios.forEach(diretorio  =>{
-      const arquivosEmBlogs = fs.readdirSync(`${raiz}/content/${diretorio.caminho}`)
-      if (arquivosEmBlogs.length > 0){
-        const itens = arquivosEmBlogs.map(nomeDoArquivo => {
-          const arquivo = fs.readFileSync(`${raiz}/content/${diretorio.caminho}/${nomeDoArquivo}`, 'utf-8')
-          const conteudo = matter(arquivo)
-  
-          return {
-            ...conteudo.data,
-            slug: nomeDoArquivo.slice(0,nomeDoArquivo.indexOf('.'))
-          }
-        })
-  
-        props[diretorio.nome] = itens;
-      }
-    })
-  
-    return {
-      props
-    }
-  }
+  return {
+    props: { membros },
+  };
+}
